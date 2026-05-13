5ccafa8464d1d01d15f659cf82dfa048b153cf5b
Meghank07 <meghank.kishan@gmail.com>
2026-05-13 12:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (26.58%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        while(left<= right){
+            if(nums[left] == target){
+                return left;
+            }
+            left++;
+        }
+       return -1;
+    }
+}
